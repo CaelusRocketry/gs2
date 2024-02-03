e589712961dcb8fd22212c14ac07eb032b5cbfed
raza <101937798+razahai@users.noreply.github.com>
2024-02-03 00:55:12 -0500

    feat(bridge): add config; create xb/sim handlers

---
+++ groundstation/dal/controllers.py
@@ -0,0 +1,43 @@
+import socket
+from time import sleep
+
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+class SimulationController():
+    def __init__(self, config: dict):
+        self.config = config
+        self.channel_layer = get_channel_layer()
+        self.host = self.config["telemetry"]["sim"]["host"]
+        self.port = self.config["telemetry"]["sim"]["port"]
+        self.bufsize = self.config["telemetry"]["sim"]["bufsize"]
+        self.delay = self.config["telemetry"]["sim"]["delay"]
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.socket.bind((self.host, self.port))
+
+    def thread(self): 
+        self.socket.listen(1)
+        (self.fs, _) = self.socket.accept() # addr doesn't matter
+
+        self.listening = True
+
+        while self.listening:
+            data: str = self.fs.recv(self.bufsize).decode()
+            async_to_sync(self.channel_layer.group_send)("ground-station", {
+                "type": "flight.data",
+                "data": data
+            })
+            sleep(self.delay)
+
+class XBeeController():
+    def __init__(self, config: dict):
+        self.config = config
+        self.baud = self.config["telemetry"]["xbee"]["baudrate"]
+        self.port = self.config["telemetry"]["xbee"]["port"]
+
+    def thread(self) -> None:
+        pass
+
--- groundstation/dal/fsbridge.py
+++ groundstation/dal/fsbridge.py
@@ -1,38 +1,23 @@
-import socket
-from time import sleep
+import json
+from os import path, getcwd
 from threading import Thread
 
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
+from .controllers import XBeeController, SimulationController
 
 class FSBridge():    
     def __init__(self):
-        self.channel_layer = get_channel_layer()
-        self.startup()
-    
-    def startup(self):
-        listener = Thread(target=self.listen_fssocket, daemon=True)
-        self.listening = True
-        listener.start() 
+        self.config: dict | None = None
+        with open(path.join(getcwd(), "conf", "config.json"), "r") as conf:
+            self.config = json.load(conf)
+         
+        self.controller: XBeeController | SimulationController | None = None
+        if self.config["environment"] == "xbee":
+            self.controller = XBeeController(self.config)
+        elif self.config["environment"] == "simulation":
+            self.controller = SimulationController(self.config) 
 
-    def listen_xbee(self) -> None:
-        # TODO: connect via xbee since socket is only for simulation
-        pass
+        self.startup()    
 
-    def listen_fssocket(self) -> None:
-        skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        skt.bind(("127.0.0.1", 5005))
-
-        skt.listen(1)
-
-        (self.fs, _) = skt.accept()
-
-        while self.listening:
-            data: str = self.fs.recv(8192).decode()
-            async_to_sync(self.channel_layer.group_send)("ground-station", {
-                "type": "flight.data",
-                "data": data
-            })
-            sleep(0.1)
+    def startup(self) -> None:
+        listen_thread = Thread(name="ListenerThread", target=self.controller.thread, daemon=True)
+        listen_thread.start() 
