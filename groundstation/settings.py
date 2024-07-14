17df05c237e9faf58640caae7adbdf5e656e4224
raza <101937798+razahai@users.noreply.github.com>
2024-07-14 02:03:12 -0400

    build: add whitenoise for static files

--- groundstation/settings.py
+++ groundstation/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROD = os.getenv('PROD', 'FALSE').upper() == 'TRUE'
-DEBUG = True # os.getenv('DEBUG', 'FALSE').upper() == 'TRUE'
+DEBUG = os.getenv('DEBUG', 'FALSE').upper() == 'TRUE'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CHANNEL_LAYERS = {
     'default': {
@@ -82,7 +89,6 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'groundstation.asgi.application'
 WSGI_APPLICATION = 'groundstation.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -130,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'groundstation', 'static')
