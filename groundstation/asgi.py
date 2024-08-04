afffa70011fd4d00730f73750e78849aa488b88b
raza <101937798+razahai@users.noreply.github.com>
2024-08-04 16:32:40 -0400

    fix: get asgi app for daphne before consumers

--- groundstation/asgi.py
+++ groundstation/asgi.py
@@ -5,11 +5,12 @@ from django.core.asgi import get_asgi_application
 from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groundstation.settings')
+django_asgi_app = get_asgi_application()
 
 from groundstation.dashboard.consumers import GroundConsumer
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': URLRouter([
         path('data/', GroundConsumer.as_asgi())
     ])
